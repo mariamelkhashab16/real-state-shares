6827b97bad93017c17d967bc8f8e23a3a1ec71ef
mariam.elkhashab <mariam.elkhashab@blnk.ai>
2025-01-13 21:53:17 +0200

    initial integration for allProperties listing screen

--- frontend/src/pages/allProperties.js
+++ frontend/src/pages/allProperties.js
@@ -1,12 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
-
+import { listProperties } from '../urls';
+import "../styles/allProperties.css"
 const AllProperties = () => {
+
+    const [properties, setProperties] = useState([])
+
+    const fetchProperties = async () => {
+        try {
+            const response = await axios.get(listProperties);
+            console.log(response.data); 
+            setProperties(response.data)
+          } catch (error) {
+            console.error('Error:', error);
+          }
+    }
+
+    useEffect(()=>{
+        fetchProperties()
+    },[])
 
   return (
     <div style={{ padding: '20px' }}>
       <h1>All Properties</h1>
+      <div>
+         {properties.map((property) => (
+          <div key={property.id} className="card">
+            <h3>{property.type}</h3>
+            {/* <p><strong>Developer:</strong> {property.floor}</p> */}
+            <p><strong>Project:</strong> {property.project_id}</p>
+
+            <p><strong>Price:</strong> EGP {property.price}</p>
+            <p><strong>Area:</strong> {property.area} m2</p>
+            <p><strong>Bedrooms:</strong> {property.bedrooms}</p>
+            <p><strong>Bathrooms:</strong> {property.bathrooms}</p>
+            <p><strong>Floor:</strong> {property.floor}</p>
+            <p>
+              <strong>Status:</strong> {property.reserved ? "Reserved" : "Available"}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ frontend/src/urls.js
@@ -0,0 +1,4 @@
+const BASE_URL = process.env.REACT_APP_BACKEND_BASE_URL;
+
+
+export const listProperties = BASE_URL + "/property"
